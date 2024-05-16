e79272a2738f34cabcd082d937c4fd03c6248793
Eduardo Franco <franc0edu24@gmail.com>
2024-05-15 21:30:38 -0300

    feat: initialize connection database and implemented expense entity

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cash_flow_db;Uid=root;Pwd=root";
+        var serverVersion = new MySqlServerVersion(new Version(8, 0, 34));
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
