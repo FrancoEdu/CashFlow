0a929d4fc5f3a99b677477020d751e7863bb31c5
Eduardo Franco <franc0edu24@gmail.com>
2024-05-22 21:43:24 -0300

    test: :white_check_mark: update delete tests
    update delete tests
    
    update delete tests

---
+++ tests/CommonTestUtilities/Requests/ExpenseDeleteRequestJsonBuilder.cs
@@ -0,0 +1,37 @@
+﻿using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests.Expense;
+using CommonTestUtilities.Jsons;
+
+namespace CommonTestUtilities.Requests;
+public class ExpenseDeleteRequestJsonBuilder
+{
+    public static List<ExpenseRegisterJson> GenerateListOfExpenses(int lenghtList)
+    {
+        List<ExpenseRegisterJson> list = new List<ExpenseRegisterJson>();
+
+        for (int i = 0; i < lenghtList; i++)
+        {
+            var newObj = new Faker<ExpenseRegisterJson>()
+            .RuleFor(r => r.Id, faker => faker.UniqueIndex)
+            .RuleFor(r => r.Title, faker => faker.Commerce.ProductName())
+            .RuleFor(r => r.Description, faker => faker.Commerce.ProductDescription())
+            .RuleFor(r => r.Date, faker => faker.Date.Past())
+            .RuleFor(r => r.PaymentType, faker => faker.PickRandom<PaymentType>())
+            .RuleFor(r => r.Amount, faker => faker.Random.Decimal());
+            list.Add(newObj);
+        }
+
+        return list;
+    }
+    public static ExpenseRegisterJson GenerateExpense()
+    {
+        return new Faker<ExpenseRegisterJson>()
+            .RuleFor(r => r.Id, faker => faker.UniqueIndex)
+            .RuleFor(r => r.Title, faker => faker.Commerce.ProductName())
+            .RuleFor(r => r.Description, faker => faker.Commerce.ProductDescription())
+            .RuleFor(r => r.Date, faker => faker.Date.Past())
+            .RuleFor(r => r.PaymentType, faker => faker.PickRandom<PaymentType>())
+            .RuleFor(r => r.Amount, faker => faker.Random.Decimal());
+    }
+}
---
+++ tests/Validators.Test/Expenses/Delete/DeleteExpenseRegisterTest.cs
@@ -0,0 +1,35 @@
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Test.Expenses.Delete;
+public class DeleteExpenseRegisterTest
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange
+        var expenses = ExpenseDeleteRequestJsonBuilder.GenerateListOfExpenses(5);
+        var expense = expenses.FirstOrDefault();
+
+        // Act
+        expenses.Remove(expense!);
+        var wasRemoved = expenses.Contains(expense!);
+
+        // Assert
+        wasRemoved.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ErrorNotFoundExpense()
+    {
+        // Arrange
+        var expenses = ExpenseDeleteRequestJsonBuilder.GenerateListOfExpenses(3);
+        var unknownExpense = ExpenseDeleteRequestJsonBuilder.GenerateExpense();
+
+        // Act
+        var exists = expenses.Contains(unknownExpense);
+        
+        // Assert
+        exists.Should().BeFalse();
+    }
+}
